cb3898fe3763dc6e4cafce4038113f91cb9dbeca
nithipat22 <wannee857@gmail.com>
2025-09-04 06:06:47 -0700

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const express = require('express')
+const express = require(`express`)
 const app = express()
-const path = require('path')
-const port = proess.evn.PORT
-const livereload = require('ivereload')
-const server = livereload.createserver()
-server.watch(__dirname + "public")
-app.use(express.stalit(path, join(__dirname, 'public')));
+const port = 80
 app.listen(port, () => {
     console.log(`app listen http://localhost:${(port)}`)
 })
