b42df1269c9387a83207128c30d38112b4c83ea4
nithipat22 <wannee857@gmail.com>
2025-09-05 01:54:51 +0700

    example html

--- public/assets/js/script.js
+++
@@ -1,3 +0,0 @@
-import { loadCDN } from "./cdnLoader";
-await loadCDN('css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css');
-await loadCDN('js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js');
